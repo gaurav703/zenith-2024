6cb216ce7903f302e12e103e38bc06ba04760626
AnkushUjawane <multiverse1808@gmail.com>
2024-02-17 10:00:21 +0530

    new page

--- src/Components/homepage/homepage.jsx
+++ src/Components/homepage/homepage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Heropage from "../heropage/heropage";
 import About from "../about/about";
+import Icons from  "../Icons/icons.js";
 import Navbar from "../navbar/navbar.jsx";
 import Mentor from "../MentorsPage/mentor.js";
 import Footer from "../footer2/footer2.jsx";
@@ -11,6 +12,7 @@ export default function Homepage() {
       <Navbar activePage="home" />
       <Heropage />
       <About />
+      <Icons />
       <Mentor />
       <Footer />
     </div>
