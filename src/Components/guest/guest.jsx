06f4683fc35103c6da956faaa600fbe7c04ac4e1
gaurav703 <111629507+gaurav703@users.noreply.github.com>
2024-02-21 01:39:47 +0530

    Update guest.jsx

--- src/Components/guest/guest.jsx
+++ src/Components/guest/guest.jsx
@@ -7,7 +7,7 @@ import React, { useState } from "react";
 
 const url = [
   {
-    img: "https://res.cloudinary.com/dqki29mbg/image/upload/v1708458779/Zenith-24/ixsbnrw62yf7zxguibp5.jpg",
+    img: "https://res.cloudinary.com/dqki29mbg/image/upload/v1708458744/Zenith-24/bwm84dbzmcwms62g1j3b.jpg",
     text: "Mr.Rishana K Devadiga",
     achive: "Captain of Maharashtra kabaddi team and Up Yoddha",
     year: "(Guest of honour Zenith'19)",
@@ -26,25 +26,25 @@ const url = [
     year: "(Guest of honour Zenith'20)",
   },
   {
-    img: "https://res.cloudinary.com/dqki29mbg/image/upload/v1708458779/Zenith-24/ixsbnrw62yf7zxguibp5.jpg",
+    img: "https://res.cloudinary.com/dqki29mbg/image/upload/v1708459688/Zenith-24/pl2guez2miasibgq1vgl.png",
     text: "Mr.Sachin Baby",
     achive: "2016-17 &2021 RCB Left hand batsman",
     year: "(Guest of honour Zenith'22)",
   },
   {
-    img: "https://res.cloudinary.com/dqki29mbg/image/upload/v1708458779/Zenith-24/ixsbnrw62yf7zxguibp5.jpg",
+    img: "https://res.cloudinary.com/dqki29mbg/image/upload/v1708458745/Zenith-24/vmpnvp1atxin1lz3icdj.png",
     text: "Mr.Sangram Choughle",
     achive: "Mr.Universe, Mr.Asia, Mr.India",
     year: "(Guest of honour Zenith'17)",
   },
   {
-    img: "https://res.cloudinary.com/dqki29mbg/image/upload/v1708458779/Zenith-24/ixsbnrw62yf7zxguibp5.jpg",
+    img: "https://res.cloudinary.com/dqki29mbg/image/upload/v1708458745/Zenith-24/euntdtajxj1exz5te9mh.png",
     text: "Mr.Unmukt Chand",
     achive: "Indian Cricketer Captain of U-19 Worldcup in 2012",
     year: "(Guest of honour Zenith'18)",
   },
   {
-    img: "https://res.cloudinary.com/dqki29mbg/image/upload/v1708458779/Zenith-24/ixsbnrw62yf7zxguibp5.jpg",
+    img: "https://res.cloudinary.com/dqki29mbg/image/upload/v1708459687/Zenith-24/yhsbo0ojfpglgjmxudjs.png",
     text: "Mr.Sambhaji Kadam",
     achive:
       "Former captain of Indian national Basketball team and Head Coach of Army team",
